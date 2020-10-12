20a41977a35fd42a3d1d12f270c548f45779d407
Julian Bonilla <julianbonilla@users.noreply.github.com>
2020-10-12 10:10:48 -0400

    config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,17 @@ if status --is-interactive
     # abbr --add --global amz 'env AWS_SECRET_ACCESS_KEY=(pass www/aws-secret-key | head -n1)'
     abbr --add --global ais "aws ec2 describe-instances | jq '.Reservations[] | .Instances[] | {iid: .InstanceId, type: .InstanceType, key:.KeyName, state:.State.Name, host:.PublicDnsName}'"
     abbr --add --global c clear
+
+    if command -v exa > /dev/null
+        abbr --add l exa
+        abbr --add ls exa
+        abbr --add ll 'exa -l'
+        abbr --add lll 'exa -la'
+    else
+        abbr --add l ls
+        abbr --add ll 'ls -l'
+        abbr --add lll 'ls -la'
+    end
 end
 
 # base16 themes
@@ -16,3 +27,4 @@ if status --is-interactive
     set fish_function_path $fish_function_path ~/projects/base16-fish/functions
     builtin source ~/projects/base16-fish/conf.d/base16.fish
 end
+
